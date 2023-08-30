ad00672113386dbfa7e5e83449d6a343d84f65e2
wjollie <william.jollie23@gmail.com>
2023-08-30 00:07:01 -0400

    -at this point im gonna do the long sleep

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,8 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-const databaseUrl = process.env.CLEARDB_DATABASE_URL;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+  dialect: "mysql",
+});
 
 module.exports = sequelize;
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
-const sequelizE = require("../config/connection");
+const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
 
 class User extends Model {
@@ -42,7 +42,7 @@ User.init(
         return updatedUserData;
       },
     },
-    sequelizE,
+    sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
