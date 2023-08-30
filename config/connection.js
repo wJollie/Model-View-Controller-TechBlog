42a637fd72ce69bc5bf3628d5c58e0fab71ee76c
wjollie <william.jollie23@gmail.com>
2023-08-29 22:55:59 -0400

    changed database information

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
+const databaseUrl = process.env.DB_URL;
+const url = require("url");
+const parsedUrl = url.parse(databaseUrl);
+const dbName = parsedUrl.pathname.replace(/^\//, ""); // Extract the database name from the path
+const dbHost = parsedUrl.hostname;
 
 let sequelize;
 
-sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+sequelize = new Sequelize(dbName, process.env.DB_USER, process.env.DB_PW, {
+  host: dbHost,
+  dialect: "mysql",
+  port: 3306,
+});
 
 module.exports = sequelize;
