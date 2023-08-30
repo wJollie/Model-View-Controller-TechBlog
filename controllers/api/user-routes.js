dd1556822335b9404c8c8405baba0caf4239c586
wjollie <william.jollie23@gmail.com>
2023-08-29 23:31:42 -0400

    -edited user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -59,6 +59,40 @@ router.post("/logout", (req, res) => {
   }
 });
 
+router.post("/signup", async (req, res) => {
+  try {
+    // Extract username and password from the request body
+    const { username, password } = req.body;
+
+    // Check if the username already exists in the database
+    const existingUser = await User.findOne({
+      where: { username },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists." });
+    }
+
+    // Create a new user in the database
+    const newUser = await User.create({
+      username,
+      password, // Note: You should hash the password before saving it to the database
+    });
+
+    // Set up the session for the new user
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.username = newUser.username;
+      req.session.loggedIn = true;
+
+      res.status(201).json({ user: newUser, message: "Signup successful." });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred during signup." });
+  }
+});
+
 // Other user routes and handlers
 
 module.exports = router;
